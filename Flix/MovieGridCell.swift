c52624f76abd6cbd116b8e27fd76ea1d4999036b
Wei <xiawei29@hotmail.com>
2022-02-26 09:35:37 -0500

    Update part 2

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Wei Xia on 2/25/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
